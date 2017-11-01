a96da2fab47246375ccde96911464911a4ad8138
Tom Elliott <tom.elliott@nyu.edu>
2017-11-01 13:18:51 -0500

    refactored script to use jsonpath

--- scripts/find_value.py
+++ scripts/find_value.py
@@ -9,12 +9,19 @@ from airtight.cli import configure_commandline
 from airtight.logging import flog
 from csv import DictWriter
 import json
+from jsonpath_ng import parse
 import logging
 from os import walk
 from os.path import abspath, join, realpath
 from pprint import pprint
 import sys
 
+MODES = [
+    'contains',
+    'startswith',
+    'endswith',
+    'equals'
+]
 DEFAULT_LOG_LEVEL = logging.WARNING
 OPTIONAL_ARGUMENTS = [
     ['-l', '--loglevel', 'NOTSET',
@@ -25,124 +32,62 @@ OPTIONAL_ARGUMENTS = [
         False],
     ['-w', '--veryverbose', False,
         'very verbose output (logging level == DEBUG)', False],
-    ['-f', '--field', '', 'what field', True],
-    ['-x', '--value', '', 'directory where to write the index', True],
-    ['-m', '--mode', 'contains', 'matching mode', True],
-    ['-s', '--case_sensitive', False, 'True or False', False],
-    ['-o', '--output', '', 'where to output CSV of results', True]
+    ['-j', '--jsonpath', '', 'a jsonpath expression', True],
+    ['-o', '--output', '', 'where to output CSV of results', False],
+    ['-m', '--mode', 'contains', 'matching mode; one of : {}'.format(MODES), True],
+    ['-t', '--target_value', '', 'value to match', True],
+    ['-s', '--case_sensitive', False, 'should matches be case-sensitive', False]
+
 ]
 POSITIONAL_ARGUMENTS = [
     # each row is a list with 3 elements: name, type, help
 ]
 
-def _sensitize(v, sensitive):
-    if sensitive:
+def _lower(v, s):
+    if s:
         return v
     else:
         return v.lower()
 
-def _dispatch(obj, field, value, case_sensitive, func):
-    first, *rest = field.split('/')
-    child = obj[first]
-    if type(child) == dict:
-        path = '/'.join(rest)
-        results = globals()[func](child, path, value, case_sensitive)
-        for r in results:
-            r['path'] = '/'.join(first, r['path'])
-        return results
-    elif type(child) == list:
-        if len(child) == 0:
-            # print('child 0')
-            return []
-        else:
-            # print('substantive child')
-            path = '/'.join(rest[1:])
-            if rest[0] != '?':
-                # print('not question mark')
-                i = int(rest[0])
-                results = globals()[func](child[i], path, value, case_sensitive)
-                # print ('not question mark results: {}'.format(len(results)))
-                for r in results:
-                    r['path'] = '/'.join(first, r['path'])
-            else:
-                # print('question mark')
-                results = []
-                for i, c in enumerate(child):
-                    rr = globals()[func](c, path, value, case_sensitive)
-                    # print('quesiton mark results: {}'.format(len(results)))
-                    for r in rr:
-                        r['path'] = '/'.join((first, '{}'.format(i), r['path']))
-                    results.extend(rr)
-            return results
+
+def _sensitize(**kwargs):
+    return (
+        _lower(getattr(kwargs['candidate'], 'value'), kwargs['case_sensitive']),
+        _lower(kwargs['target_value'], kwargs['case_sensitive'])
+        )
 
 
-def _contains(obj, field, value, case_sensitive, **kwargs):
-    # print('field: {}'.format(field))
-    if '/' in field:
-        return _dispatch(obj, field, value, case_sensitive, '_contains')
-    quarry = _sensitize(obj[field], case_sensitive)
-    target = _sensitize(value, case_sensitive)
-    # print('quarry: {}'.format(quarry))
-    # print('target: {}'.format(target))
+def _contains(**kwargs):
+    quarry, target = _sensitize(**kwargs)
     if target in quarry:
-        d = {
-            'path': field,
-            'value': value,
-            'match': obj[field],
-            'sensitive': case_sensitive
-        }
-        # print('boom')
-        return [d]
+        return [kwargs['candidate']]
     else:
         return []
 
-def _startswith(obj, field, value, case_sensitive, **kwargs):
-    if '/' in field:
-        return _dispatch(obj, field, value, case_sensitive, '_startswith')
-    quarry = _sensitize(obj[field], case_sensitive)
-    target = _sensitize(value, case_sensitive)
+
+def _startswith(**kwargs):
+    quarry, target = _sensitize(**kwargs)
     if quarry.startswith(target):
-        d = {
-            'path': field,
-            'value': value,
-            'match': obj[field],
-            'sensitive': case_sensitive
-        }
-        return [d]
+        return [kwargs['candidate']]
     else:
         return []
+
 
 def _endswith(obj, field, value, case_sensitive, **kwargs):
-    if '/' in field:
-        return _dispatch(obj, field, value, case_sensitive, '_endswith')
-    quarry = _sensitize(obj[field], case_sensitive)
-    target = _sensitize(value, case_sensitive)
+    quarry, target = _sensitize(**kwargs)
     if quarry.endswith(target):
-        d = {
-            'path': field,
-            'value': value,
-            'match': obj[field],
-            'sensitive': case_sensitive
-        }
-        return [d]
+        return [kwargs['candidate']]
     else:
         return []
 
+
 def _exact(obj, field, value, case_sensitive, **kwargs):
-    if '/' in field:
-        return _dispatch(obj, field, value, case_sensitive, '_exact')
-    quarry = _sensitize(obj[field], case_sensitive)
-    target = _sensitize(value, case_sensitive)
+    quarry, target = _sensitize(**kwargs)
     if quarry == target:
-        d = {
-            'path': field,
-            'value': value,
-            'match': obj[field],
-            'sensitive': case_sensitive
-        }
-        return [d]
+        return [kwargs['candidate']]
     else:
         return []
+
 
 def main(**kwargs):
     """
@@ -150,35 +95,46 @@ def main(**kwargs):
     """
     # logger = logging.getLogger(sys._getframe().f_code.co_name)
     path = abspath(realpath(join('data', 'json')))
-    hits = []
+    jrx = parse(kwargs['jsonpath'])
+    hits = {}
+    files = 0
     for dirpath, dirnames, filenames in walk(path):
         for filename in filenames:
             if filename.split('.')[-1] == 'json':
                 filepath = join(dirpath, filename)
                 with open(filepath, 'r') as f:
                     p = json.load(f)
-                results = globals()['_{}'.format(kwargs['mode'])](p, **kwargs)
-                if results is not None:
-                    for r in results:
-                        r['place_uri'] = p['uri']
-                    if kwargs['output'] == '':
-                        for r in results:
-                            print('{')
-                            for k,v in r.items():
-                                print("\t'{}': '{}'".format(k, v))
-                            print('}')
-                    hits.extend(results)
-            # print('hits: {}'.format(len(hits)))
-            # sys.exit(0)
+                files += 1
+                candidates = jrx.find(p)
+                matches = []
+                for c in candidates:
+                    matches = globals()['_{}'.format(kwargs['mode'])](candidate=c, **kwargs)
+                pid = p['uri'].split('/')[-1]
+                for m in matches:
+                    try:
+                        hits[pid].append(m)
+                    except KeyError:
+                        hits[pid] = [m]
+                if files % 1000 == 0:
+                    print(
+                        'Checked {} files. {} matches so far.'
+                        ''.format(files, len(hits)))
     if kwargs['output'] != '':
         if len(hits) == 0:
             print('No results found.')
         else:
             where = abspath(realpath(kwargs['output']))
-            filepath = join(where, 'found_values.json')
-            with open (filepath, 'w', encoding="utf-8") as f:
+            if not(where.endswith('.json')):
+                where = join(where, 'found_values.json')
+            with open (where, 'w', encoding="utf-8") as f:
                 json.dump(hits, f, indent=4, sort_keys=True, ensure_ascii=False)
             print('wrote {} results to {}'.format(len(hits), filepath))
+    else:
+        print('{} hits'.format(len(hits)))
+        for pid, matches in hits.items():
+            for match in matches:
+                print('{}[{}]: "{}"'.format(pid, match.full_path, match.value))
+
 
 if __name__ == "__main__":
     main(**configure_commandline(
